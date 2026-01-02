b7dad25d8844de0d6980ef8095e5d0bb75a8fc26
ArtemPotapemko <potapartemka@yandex.ru>
2026-01-02 22:06:57 +0300

    fix: update test

---
+++ src/test/kotlin/ru/itmo/hls/ordermanager/controller/PaginationValidatorTest.kt
@@ -0,0 +1,29 @@
+package ru.itmo.hls.ordermanager.controller
+
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+
+@SpringBootTest(classes = [PaginationValidator::class])
+@TestPropertySource(properties = ["spring.application.pagination.max-size=5"])
+class PaginationValidatorTest {
+
+    @Autowired
+    private lateinit var paginationValidator: PaginationValidator
+
+    @Test
+    fun validateSizeAcceptsPositiveValues() {
+        assertDoesNotThrow { paginationValidator.validateSize(1) }
+        assertDoesNotThrow { paginationValidator.validateSize(5) }
+    }
+
+    @Test
+    fun validateSizeRejectsTooLarge() {
+        assertThrows(IllegalArgumentException::class.java) {
+            paginationValidator.validateSize(6)
+        }
+    }
+}
